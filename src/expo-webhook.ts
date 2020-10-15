677130ee91281772e285dcb8a4a9400c70a94376
Atch <atcheri@laposte.net>
2020-10-15 11:47:24 +0200

    adapt expo webhook example

--- src/expo-webhook.ts
+++ src/expo-webhook.ts
@@ -1,13 +1,58 @@
-import { APIGatewayEvent, Context } from 'aws-lambda';
+import { APIGatewayEvent /*, Callback, Context*/ } from 'aws-lambda';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
+import crypto from 'crypto';
+import safeCompare from 'safe-compare';
 
-export async function handler(event: APIGatewayEvent, context: Context) {
-  const { msg } = event.queryStringParameters;
+type LambdaResponse = {
+  statusCode: number;
+  headers?: { [name: string]: string };
+  body: string;
+};
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ msg })
-  };
+export async function handler(
+  event: APIGatewayEvent
+  /* context: Context, callback: Callback*/
+): Promise<LambdaResponse> {
+  if (event.httpMethod !== 'POST') {
+    return createErrorResponse(StatusCodes.FORBIDDEN);
+  }
+  try {
+    const { body, headers } = event;
+    if (!body || !process.env.EXPO_WEBHOOK_SECRET) {
+      return createErrorResponse(StatusCodes.NO_CONTENT);
+    }
+    const hmac = crypto
+      .createHmac('sha1', process.env.EXPO_WEBHOOK_SECRET)
+      .update(body);
+    const hash = `sha1=${hmac.digest('hex')}`;
+    const expoSignature = headers['expo-signature'];
+    if (!safeCompare(expoSignature, hash)) {
+      return createErrorResponse(StatusCodes.UNAUTHORIZED);
+    }
+    const { msg } = JSON.parse(body);
+    return createSuccessResponse({ msg });
+  } catch (e) {
+    console.log(e);
+    return createErrorResponse(StatusCodes.EXPECTATION_FAILED);
+  }
 }
+
+const createSuccessResponse = (body: Record<string, unknown>) => {
+  return createResponse(StatusCodes.OK, body);
+};
+
+const createErrorResponse = (code: number) => {
+  return createResponse(code, [getReasonPhrase(code)]);
+};
+
+const createResponse = (
+  code: number,
+  body: string[] | Record<string, unknown>,
+  headers = {}
+): LambdaResponse => {
+  return {
+    statusCode: code,
+    body: JSON.stringify(body),
+    headers: Object.assign(headers, { 'Content-Type': 'application/json' })
+  };
+};
