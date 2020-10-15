a5ce72f35e4e7da39dc451ec3d0d4fb1ef796f60
Atch <atcheri@laposte.net>
2020-10-15 15:51:38 +0200

    modify the simple function to log the post data

--- src/simple_get.ts
+++ src/simple_get.ts
@@ -3,6 +3,14 @@ import { StatusCodes } from 'http-status-codes';
 
 export async function handler(event: APIGatewayEvent) {
   const { name = 'test' } = event.queryStringParameters;
+  if (event.httpMethod === 'POST') {
+    console.log('headers', event.headers);
+    console.log('body', event.body);
+    return {
+      statusCode: StatusCodes.OK,
+      body: event.body
+    };
+  }
   return {
     statusCode: StatusCodes.OK,
     body: JSON.stringify({ name })
